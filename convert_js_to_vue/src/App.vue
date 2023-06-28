6b1af8435be0fe99aedf9c643c630128c6e3a786
KeenoFourie <keenofourie614@gmail.com>
2023-06-28 14:13:38 +0200

    pages made

---
+++ convert_js_to_vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<NavBarComp/>
+<router-view/>
+<FooterComp/>
+</template>
+
+<script>
+import NavBarComp from "@/components/NavBarComp.vue"
+import FooterComp from "@/components/FooterComp.vue"
+export default {
+  name: 'HomeView',
+  components: {
+    NavBarComp,
+    FooterComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
